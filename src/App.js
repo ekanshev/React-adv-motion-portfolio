26a8a96c26640a4df62b46668aa6f51cdc650413
EKansh <ekanshev.06@gmail.com>
2022-07-22 23:00:43 +0530

    Added component and sub component and added functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, useLocation } from "react-router"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+
+
+//Components
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import BlogPage from './components/BlogPage';
+import WorkPage from './components/WorkPage';
+import MySkillsPage from './components/MySkillsPage';
+import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const location = useLocation();
+  return <>
+
+
+  <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+
+    <SoundBar />
+
+{/* For framer-motion animation on page change! */}
+<AnimatePresence exitBeforeEnter>
+<Routes  location={location} key={location.pathname}>
+      <Route exact path="/" component={Main}/>
+      <Route exact path="/about" component={AboutPage}/>
+      <Route exact path="/blog" component={BlogPage}/>
+      <Route exact path="/work" component={WorkPage}/>
+      <Route exact path="/skills" component={MySkillsPage}/>
+
+    </Routes>
+</AnimatePresence>
+    
+    
+    </ThemeProvider>
+
+
+    
+    </>
+    
 }
 
-export default App;
+export default App
